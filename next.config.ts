3a30e3bcfbdf36fd9a13da57d4b9edccf57d583c
ASUS <2243409205@qq.com>
2026-05-11 16:25:24 +0800

    关闭PPR，让项目能够在Vercel部署

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  ppr: false,
 };
 
 export default withNextIntl(nextConfig);
